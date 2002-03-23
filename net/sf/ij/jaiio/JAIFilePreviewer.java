adc26bdb389dd779d9ef9829493d04fff6811c16
Jarek Sacha <jpsacha@gmail.com>
2002-03-23 20:25:46 +0000

    Fix problem with missaligned image and image info.

--- net/sf/ij/jaiio/JAIFilePreviewer.java
+++ net/sf/ij/jaiio/JAIFilePreviewer.java
@@ -180,7 +180,7 @@ public class JAIFilePreviewer extends JPanel
     infoPanel.setLayout(borderLayout1);
     ImageIconLabel.setPreferredSize(new Dimension(200, 150));
     ImageIconLabel.setHorizontalAlignment(SwingConstants.CENTER);
-    this.add(infoPanel,  new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0
+    this.add(infoPanel,   new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0
             ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
     infoPanel.add(fileSizeLabel, BorderLayout.CENTER);
     this.add(ImageIconLabel,   new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0
