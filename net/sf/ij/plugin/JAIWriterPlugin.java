16cc0f73ac9f48f058e8e76075b433a0096ba3b7
Jarek Sacha <jpsacha@gmail.com>
2003-01-19 01:59:07 +0000

    Macro scripting support.

--- net/sf/ij/plugin/JAIWriterPlugin.java
+++ net/sf/ij/plugin/JAIWriterPlugin.java
@@ -1,6 +1,6 @@
 /*
  * Image/J Plugins
- * Copyright (C) 2002 Jarek Sacha
+ * Copyright (C) 2002,2003 Jarek Sacha
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -23,8 +23,10 @@ package net.sf.ij.plugin;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.WindowManager;
+import ij.Macro;
 import ij.io.OpenDialog;
 import ij.plugin.PlugIn;
+import ij.plugin.frame.Recorder;
 import ij.process.ImageProcessor;
 
 import java.io.File;
@@ -32,6 +34,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
+
 import net.sf.ij.jaiio.JAIFileChooserFactory;
 import net.sf.ij.jaiio.JAIFileFilter;
 import net.sf.ij.jaiio.JAIWriter;
@@ -41,68 +44,120 @@ import net.sf.ij.jaiio.JAIWriter;
  *
  * @author     Jarek Sacha
  * @created    March 3, 2002
- * @version    $Revision: 1.3 $
+ * @version    $Revision: 1.4 $
  */
 
 public class JAIWriterPlugin implements PlugIn {
+
+  private static final String MACRO_OPTION_FILENAME = "JAIWriter.fileName";
+  private static final String MACRO_OPTION_CODECNAME = "JAIWriter.codecName";
 
   private static JFileChooser jaiChooser;
 
 
   /**
-   *  Main processing method for the JAIReaderPlugin object
+   *  Main processing method for the JAIWriterPlugin object
    *
-   * @param  arg  Argument.
+   * @param  arg  Not used.
    */
   public void run(String arg) {
-    File file = null;
     ImagePlus imp = WindowManager.getCurrentImage();
     if (imp == null) {
       IJ.showMessage("JAI Writer", "No images are open.");
       return;
     }
 
-    try {
+    String fileName = null;
+    String codecName = null;
+
+    // Check if macro options are present
+    String macroOptions = Macro.getOptions();
+    if (macroOptions != null) {
+      fileName = Macro.getValue(macroOptions, MACRO_OPTION_FILENAME, fileName);
+      codecName = Macro.getValue(macroOptions, MACRO_OPTION_CODECNAME, codecName);
+    }
+
+    // Sanity checks
+    if (fileName != null && codecName == null) {
+      IJ.showMessage("JAI Writer Plugin",
+          "Macro option '" + MACRO_OPTION_CODECNAME + "' is missing");
+      Macro.abort();
+      return;
+    } else if (fileName == null && codecName != null) {
+      IJ.showMessage("JAI Writer Plugin",
+          "Macro option '" + MACRO_OPTION_FILENAME + "' is missing");
+      Macro.abort();
+      return;
+    }
+
+
+    if (fileName == null && codecName == null) {
+      // Get fileName and codecName showing save dialog
+//        try {
       if (jaiChooser == null) {
         jaiChooser = JAIFileChooserFactory.createJAISaveChooser();
         jaiChooser.setCurrentDirectory(new File(OpenDialog.getDefaultDirectory()));
       }
 
-      jaiChooser.setName(imp.getTitle());
+      File file = new File(imp.getTitle());
+      jaiChooser.setSelectedFile(file);
 
-      if (jaiChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
-        FileFilter fileFilter = jaiChooser.getFileFilter();
-        String codecName = null;
-        if (fileFilter instanceof JAIFileFilter) {
-          JAIFileFilter jaiFileFilter = (JAIFileFilter) fileFilter;
-          codecName = jaiFileFilter.getCodecName();
-        }
-
-        if (codecName == null) {
-          IJ.showMessage("JAI Writer", "File format not selected. File not saved.");
-          return;
-        }
-
-        JAIWriter jaiWriter = new JAIWriter();
-        jaiWriter.setFormatName(codecName);
-        file = jaiChooser.getSelectedFile();
-        if (file.getName().indexOf(".") < 0) {
-          file = new File(file.getParent(),
-              file.getName() + "." + getFileExtension(codecName));
-        }
-        jaiWriter.write(file.getAbsolutePath(), imp);
+      if (jaiChooser.showSaveDialog(null) != JFileChooser.APPROVE_OPTION) {
+        Macro.abort();
+        return;
       }
+
+      FileFilter fileFilter = jaiChooser.getFileFilter();
+      if (fileFilter instanceof JAIFileFilter) {
+        JAIFileFilter jaiFileFilter = (JAIFileFilter) fileFilter;
+        codecName = jaiFileFilter.getCodecName();
+        Recorder.recordOption("JAIWriter.codecName", codecName);
+      }
+
+      if (codecName == null) {
+        IJ.showMessage("JAI Writer", "File format not selected. File not saved.");
+        Macro.abort();
+        return;
+      }
+
+      file = jaiChooser.getSelectedFile();
+      if (file.getName().indexOf(".") < 0) {
+        file = new File(file.getParent(),
+            file.getName() + "." + getFileExtension(codecName));
+      }
+      fileName = file.getAbsolutePath();
+      Recorder.recordOption("JAIWriter.fileName", fileName);
+//    } catch (Throwable t) {
     }
-//    catch(FileNotFoundException ex) {
-//    }
-//    catch(IOException ex) {
-//    }
-    catch (Throwable t) {
-      t.printStackTrace();
-      String msg = "Error writing file: " + file.getName() + ".\n\n";
-      msg += (t.getMessage() == null) ? t.toString() : t.getMessage();
+
+    //
+    // Now ready to write the image to a file
+    //
+    try {
+      write(imp, fileName, codecName);
+    } catch (IOException e) {
+      e.printStackTrace();
+      Macro.abort();
+      String msg = "Error writing file: " + fileName + ".\n\n";
+      msg += (e.getMessage() == null) ? e.toString() : e.getMessage();
       IJ.showMessage("JAI Writer", msg);
     }
+  }
+
+
+  /**
+   *
+   * @param imp
+   * @param fileName
+   * @param codecName
+   * @throws IOException
+   * @throws FileNotFoundException
+   */
+  private static void write(ImagePlus imp, String fileName, String codecName)
+      throws IOException, FileNotFoundException {
+    JAIWriter jaiWriter = new JAIWriter();
+    jaiWriter.setFormatName(codecName);
+    jaiWriter.write(fileName, imp);
   }
 
 
@@ -112,11 +167,9 @@ public class JAIWriterPlugin implements PlugIn {
   private String getFileExtension(String codecName) {
     if (codecName.compareToIgnoreCase("TIFF") == 0) {
       return "tif";
-    }
-    else if (codecName.compareToIgnoreCase("JPEG") == 0) {
+    } else if (codecName.compareToIgnoreCase("JPEG") == 0) {
       return "jpg";
-    }
-    else {
+    } else {
       return codecName.toLowerCase();
     }
   }
