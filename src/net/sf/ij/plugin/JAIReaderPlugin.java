8f5c56577aeff1fdbfffbb2bd542eb1d312bbb0a
Jarek Sacha <jpsacha@gmail.com>
2004-01-08 03:55:43 +0000

    Use new argument values, can help to determine if an obsolete class is loaded (open with preview will not be displayed by old classes).

--- src/net/sf/ij/plugin/JAIReaderPlugin.java
+++ src/net/sf/ij/plugin/JAIReaderPlugin.java
@@ -41,7 +41,7 @@ import net.sf.ij.jaiio.JAIReader;
  *
  * @author     Jarek Sacha
  * @created    February 10, 2002
- * @version    $Revision: 1.1 $
+ * @version    $Revision: 1.2 $
  */
 
 public class JAIReaderPlugin implements PlugIn {
@@ -55,7 +55,7 @@ public class JAIReaderPlugin implements PlugIn {
    *  Argument passed to <code>run</code> method to use open dialog with an
    *  image preview.
    */
-  public final static String ARG_IMAGE_PREVIEW = "image preview";
+  public final static String ARG_IMAGE_PREVIEW = "preview";
 
   private static ImageFileChooser jaiChooser;
   private int[] pageIndex;
