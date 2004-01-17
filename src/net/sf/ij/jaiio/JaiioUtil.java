8c812018152d92887a33e8478609cd912417f336
Jarek Sacha <jpsacha@gmail.com>
2004-01-17 05:33:53 +0000

    Add centerOnScreen method

--- src/net/sf/ij/jaiio/JaiioUtil.java
+++ src/net/sf/ij/jaiio/JaiioUtil.java
@@ -2,6 +2,7 @@ package net.sf.ij.jaiio;
 
 import ij.process.ImageProcessor;
 
+import java.awt.*;
 import java.awt.image.ColorModel;
 import java.awt.image.IndexColorModel;
 
@@ -16,11 +17,45 @@ public class JaiioUtil {
      */
     public static boolean isBinary(ImageProcessor ip) {
         ColorModel cm = ip.getColorModel();
-        if (cm instanceof IndexColorModel
-                && ((IndexColorModel) cm).getMapSize() == 2) {
-            return true;
+        if (cm instanceof IndexColorModel) {
+            int mapSize = ((IndexColorModel) cm).getMapSize();
+            if (mapSize == 2) {
+                return true;
+            }
         }
 
         return false;
+    }
+
+    /**
+     * Center window on screen.
+     *
+     * @param window
+     * @param packFrame if <code>true</code> call window's <code>pack()</code>
+     *                  method before centering.
+     */
+    public static void centerOnScreen(Window window, boolean packFrame) {
+        //Validate frames that have preset sizes
+        //Pack frames that have useful preferred size info, e.g. from their layout
+        if (packFrame) {
+            window.pack();
+        } else {
+            window.validate();
+        }
+
+        //Center the frame window
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension frameSize = window.getSize();
+
+        if (frameSize.height > screenSize.height) {
+            frameSize.height = screenSize.height;
+        }
+
+        if (frameSize.width > screenSize.width) {
+            frameSize.width = screenSize.width;
+        }
+
+        window.setLocation((screenSize.width - frameSize.width) / 2,
+                (screenSize.height - frameSize.height) / 2);
     }
 }
