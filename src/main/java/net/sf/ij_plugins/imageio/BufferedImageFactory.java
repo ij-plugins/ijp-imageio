183c046db24065ece10db5125b793f0decf93f6c
Jarek Sacha <jpsacha@gmail.com>
2020-02-12 20:51:08 -0500

    Correct javadoc

--- src/main/java/net/sf/ij_plugins/imageio/BufferedImageFactory.java
+++ src/main/java/net/sf/ij_plugins/imageio/BufferedImageFactory.java
@@ -1,23 +1,23 @@
 /*
- * Image/J Plugins
- * Copyright (C) 2002-2016 Jarek Sacha
- * Author's email: jpsacha at gmail.com
+ *  IJ Plugins
+ *  Copyright (C) 2002-2020 Jarek Sacha
+ *  Author's email: jpsacha at gmail.com
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Latest release available at http://sourceforge.net/projects/ij-plugins/
+ *  Latest release available at https://github.com/ij-plugins/ijp-imageio
  */
 
 package net.sf.ij_plugins.imageio;
@@ -214,12 +214,12 @@ public class BufferedImageFactory {
      * Create BufferedImage from ByteProcessor, use IndexColorModel with uniform gray level distribution from 0 to 255.
      * Share pixels with source.
      * <p>
-     * Alternative approach is to use {@link ij.process.ByteProcessor#createBufferedImage()} which uses color model
+     * Alternative approach is to use {@link ij.process.ByteProcessor#getBufferedImage()} which uses color model
      * currently assigned to source ByteProcessor.
      *
      * @param src ByteProcessor source.
      * @return BufferedImage.
-     * @see ij.process.ByteProcessor#createBufferedImage()
+     * @see ij.process.ByteProcessor#getBufferedImage()
      */
     public static BufferedImage createFrom(final ByteProcessor src) {
         return createFrom(src, false);
