4380ef44cf2c50a152fd9ac9b8c9b4c7bd143aaa
Jarek Sacha <jpsacha@gmail.com>
2020-02-13 21:39:05 -0500

    Cleanup

--- src/test/java/net/sf/ij_plugins/imageio/ImageIODemo.java
+++ src/test/java/net/sf/ij_plugins/imageio/ImageIODemo.java
@@ -17,7 +17,7 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  Latest release available at http://sourceforge.net/projects/ij-plugins/
+ *  Latest release available at https://github.com/ij-plugins/ijp-imageio
  */
 package net.sf.ij_plugins.imageio;
 
@@ -35,27 +35,28 @@ import java.util.TreeMap;
 public class ImageIODemo {
     public static void main(String[] args) {
         describeWriters();
+        System.out.println("\n---------------------------------\n");
+        listAll();
     }
 
     public static void listAll() {
-        String readerNames[] = ImageIO.getReaderFormatNames();
+        String[] readerNames = ImageIO.getReaderFormatNames();
         printList("Reader Names:", readerNames);
 
-        String readerMimes[] = ImageIO.getReaderMIMETypes();
+        String[] readerMimes = ImageIO.getReaderMIMETypes();
         printList("Reader MIME types:", readerMimes);
 
-        String writerNames[] = ImageIO.getWriterFormatNames();
+        String[] writerNames = ImageIO.getWriterFormatNames();
         printList("Writer Names:", writerNames);
 
-        String writerMimes[] = ImageIO.getWriterMIMETypes();
+        String[] writerMimes = ImageIO.getWriterMIMETypes();
         printList("Writer MIME types:", writerMimes);
     }
 
     public static void describeWriters() {
-        String writerNames[] = getUniqueWriterFormatNames();
+        String[] writerNames = getUniqueWriterFormatNames();
         //        printList("Writer Names:", writerNames);
-        for (int i = 0; i < writerNames.length; i++) {
-            String writerName = writerNames[i];
+        for (String writerName : writerNames) {
             System.out.println(writerName + " writers:");
             Iterator<ImageWriter> iterator = ImageIO.getImageWritersByFormatName(writerName);
             while (iterator.hasNext()) {
@@ -87,21 +88,21 @@ public class ImageIODemo {
                     //                System.out.println("\tDefault compression quality: "+param.getCompressionQuality());
                     String[] compressionTypes = param.getCompressionTypes();
                     System.out.print("\tQuality descriptors: ");
-                    for (int j = 0; j < compressionTypes.length; j++) {
-                        System.out.print(compressionTypes[j] + ", ");
+                    for (String compressionType : compressionTypes) {
+                        System.out.print(compressionType + ", ");
                     }
-                    System.out.println("");
+                    System.out.println();
 
-                    for (int j = 0; j < compressionTypes.length; j++) {
-                        param.setCompressionType(compressionTypes[j]);
+                    for (String compressionType : compressionTypes) {
+                        param.setCompressionType(compressionType);
                         String[] qualityDescriptors = param.getCompressionQualityDescriptions();
                         float[] qualityValues = param.getCompressionQualityValues();
                         if (qualityDescriptors != null && qualityDescriptors.length > 0) {
-                            System.out.print("\tCompression quality descriptors for " + compressionTypes[j] + ": ");
+                            System.out.print("\tCompression quality descriptors for " + compressionType + ": ");
                             for (int k = 0; k < qualityDescriptors.length; k++) {
                                 System.out.print(qualityDescriptors[k] + "[" + qualityValues[k] + "], ");
                             }
-                            System.out.println("");
+                            System.out.println();
                         }
                     }
                 }
@@ -112,7 +113,7 @@ public class ImageIODemo {
     }
 
     private static String[] getUniqueWriterFormatNames() {
-        String writerNames[] = ImageIO.getWriterFormatNames();
+        String[] writerNames = ImageIO.getWriterFormatNames();
         Map<String, String> map = new TreeMap<>();
         for (String writerName : writerNames) {
             Iterator<ImageWriter> iterator = ImageIO.getImageWritersByFormatName(writerName);
@@ -125,10 +126,10 @@ public class ImageIODemo {
         }
 
         Collection<String> values = map.values();
-        return values.toArray(new String[values.size()]);
+        return values.toArray(new String[0]);
     }
 
-    private static void printList(String title, String[] list) {
+    static void printList(String title, String[] list) {
         System.out.println(title);
         for (String aList : list) {
             System.out.println("\t" + aList);
